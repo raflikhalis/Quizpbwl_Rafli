87faace885a2113ac3ea11d3b697e5def9b92dfc
raflikhalis <rafli20nugraha@gmail.com>
2023-07-06 22:39:19 +0700

    first commit

---
+++ resources/views/dashboard/main_dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('dashboard.template')
+
+@section('content')
+    <div class="col-md-9">
+        <div class="card">
+        <div class="card-header">
+            Dashboard
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Welcome MR. RAFLI KHALIS NUGRAHA </h5>
+        </div>
+        </div>
+    </div>
+@endsection
